6b4e170283d44afc4df4d89de54ab593821b45fe
zhuangjt <zhuangjt@zhuangjt.local>
2018-04-24 16:40:56 +0800

    version 1.0.2

--- src/test/java/com/sien/es/EsApplicationTests.java
+++ src/test/java/com/sien/es/EsApplicationTests.java
@@ -2,21 +2,14 @@ package com.sien.es;
 
 import com.alibaba.fastjson.JSON;
 import com.sien.entity.City;
-import com.sien.entity.Principal;
 import com.sien.es.dao.CitySearchRepository;
-import com.sien.es.dao.PrincipalSearchRepository;
 import com.sien.service.DataImportExecutor;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
-import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
-import org.springframework.data.elasticsearch.core.query.SearchQuery;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.Optional;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
